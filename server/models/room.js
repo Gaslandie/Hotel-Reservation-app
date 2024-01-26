342d8d30a7ae13177f5500e42be72fe1e79a38f2
Gassama <gassma803@gmail.com>
2024-01-26 14:34:49 +0100

    configuration de base du projet

--- server/models/room.js
+++ server/models/room.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const roomSchema = new mongoose.Schema({
     name:String,
-    price:Number
+    price:Number,
+    booked:Boolean,
+    size:String
 })
 
 module.exports = mongoose.model('Room',roomSchema)
